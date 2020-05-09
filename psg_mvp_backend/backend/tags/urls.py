9a765647c432fc18dc580d8753ee87944aafa47f
Erin <hchings@gmail.com>
2020-05-09 15:23:02 -0700

    3BD-22 fix img resize and add catalog API
    - add catalog
    - fix: only resize on height for other images
    - serve scp_user_pic as base64 through API

--- psg_mvp_backend/backend/tags/urls.py
+++ psg_mvp_backend/backend/tags/urls.py
@@ -6,7 +6,7 @@ Urls for tags for all features.
 from django.conf.urls import url, include
 
 
-from .views import ClinicNameListView
+from .views import ClinicNameListView, SurgeryListView
 
 internal_apis = [
 
@@ -14,4 +14,5 @@ internal_apis = [
 
 urlpatterns = [
     url(r'^clinics_names/$', ClinicNameListView.as_view(), name=ClinicNameListView.name),
+    url(r'^surgeries/$', SurgeryListView.as_view(), name=SurgeryListView.name),
 ]
