4e362824710b593d34e700aab8dae03d727d3e11
Christian Joun <cjoun@C02YD47QJG5J.corp.proofpoint.com>
2021-09-25 17:49:52 -0700

    update run_all.py to work with new facebook puppetter scrapper

--- psg_mvp_backend/backend/scripts/scrapper/run_all.py
+++ psg_mvp_backend/backend/scripts/scrapper/run_all.py
@@ -10,22 +10,21 @@ if __name__ == '__main__':
         data = json.load(f)
 
     for idx, clinic in enumerate(data["results"]):
-        #if idx > 2:
+        # if idx == 0:
+        #     continue
+        # if idx > 2 :
         #    break
-        # print(clinic)
         clinic_uuid = clinic["clinic_uuid"] or "missing"
         branch = clinic["branch"] or "missing"
         place_id = clinic["placeId"] or "missing"
         url = clinic["url"] or "missing"
 
-
         if clinic["source"] == "facebook":
-            print("we have facebook")
-            print('cd fb_map_review; python fb_scrapper.py %s "%s"' % (clinic_uuid, url))
-            os.system('cd fb_map_review; python fb_scrapper.py %s "%s"' % (clinic_uuid, url))
+            print('facebook clinic idx=%s; node index.js --url "%s" --uuid "%s"' % (idx, url, clinic_uuid))
+            os.system('cd puppeteer-login-for-facebook; node index.js --url "%s" --uuid "%s"' % (url, clinic_uuid))
         if clinic["source"] == "google":
             continue
-            print("we have google")
+            print('google clinic idx=%s; node scrape.js %s %s %s "%s"' % (clinic_uuid, branch, place_id, url))
             os.system('cd gl_map_review; node scrape.js %s %s %s "%s"' % (clinic_uuid, branch, place_id, url))
 
         # if i >= 100:
